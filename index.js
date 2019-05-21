6c64b06c93acf13dec2b467d6ef52488cf92c087
MinimalistYing <genius_ying@163.com>
2019-05-21 15:48:23 +0800

    isJSON

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import isURL from './isURL'
 import isEnter from './isEnter'
 import formatJSON from './formatJSON'
+import isJSON from './isJSON'
 
 export default {
   isURL,
   isEnter,
-  formatJSON
+  formatJSON,
+  isJSON
 }
--- isEnter.js
+++ isEnter.js
@@ -5,5 +5,6 @@
  */
 export default function isEnter(e) {
   if (typeof e !== 'object') return false
+
   return e.key === 'Enter' || e.which === 13 || e.keyCode === 13
 }
