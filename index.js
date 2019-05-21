9298e4300d50fedc255ce1f9b8204cf7424efba7
MinimalistYing <genius_ying@163.com>
2019-05-21 15:17:38 +0800

    style.css

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import isURL from './isURL'
+import isEnter from './isEnter'
+import formatJSON from './formatJSON'
 
 export default {
-  isURL
+  isURL,
+  isEnter,
+  formatJSON
 }
--- isEnter.js
+++ isEnter.js
@@ -4,5 +4,6 @@
  * @param {event} e 
  */
 export default function isEnter(e) {
+  if (typeof e !== 'object') return false
   return e.key === 'Enter' || e.which === 13 || e.keyCode === 13
 }
