d4479c5c27a3fa9da3a318177c6772b320b720b2
MinimalistYing <genius_ying@163.com>
2019-12-04 15:17:26 +0800

    fix https://github.com/MinimalistYing/Bape/issues/2

--- isURL.js
+++ isURL.js
@@ -26,7 +26,7 @@
   function isURL(str) {
     if (typeof str !== 'string') return false; // From https://stackoverflow.com/a/8234912/8459774
 
-    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/i.test(str);
+    return /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/i.test(str);
   }
 
   module.exports = exports.default;
